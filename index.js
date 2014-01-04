afd64fd53d5c8864aef8e15a92d45b45d68791a4
TJ Holowaychuk <tj@vision-media.ca>
2014-01-03 20:13:57 -0800

    remove redundant .status set for 404

--- index.js
+++ index.js
@@ -33,9 +33,7 @@ function error(opts) {
   return function *error(next){
     try {
       yield next;
-      // Handle 404 upstream.
-      var status = this.status || 404;
-      if (404 == status) this.throw(404);
+      if (404 == this.status) this.throw(404);
     } catch (err) {
       this.status = err.status || 500;
 
