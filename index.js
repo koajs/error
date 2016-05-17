9738effa7ebdcc513d701225e4c8b9fac80c61b1
Daniel Friesen <daniel@nadir-seen-fire.com>
2016-05-17 08:25:13 -0700

    Update to Koa 2 and refactor code
    Note: consolidate supports promises so it is now used directly instead of through co-render

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * Module dependencies.
  */
 
-const render = require('co-render');
+const consolidate = require('consolidate');
 const http = require('http');
 
 /**
@@ -37,48 +37,49 @@ function error(opts) {
   var cache = opts.cache;
   if (null == cache) cache = 'development' != env;
 
-  return function *error(next){
-    try {
-      yield next;
-      if (404 == this.response.status && !this.response.body) this.throw(404);
-    } catch (err) {
-      this.status = err.status || 500;
+  return function error(ctx, next){
+    return next()
+      .then(() => {
+        if (404 == ctx.response.status && !ctx.response.body) ctx.throw(404);
+      })
+      .catch(err => {
+        ctx.status = err.status || 500;
 
-      // application
-      this.app.emit('error', err, this);
+        // application
+        ctx.app.emit('error', err, ctx);
 
-      // accepted types
-      switch (this.accepts('html', 'text', 'json')) {
-        case 'text':
-          this.type = 'text/plain';
-          if ('development' == env) this.body = err.message
-          else if (err.expose) this.body = err.message
-          else throw err;
-          break;
+        // accepted types
+        switch (ctx.accepts('html', 'text', 'json')) {
+          case 'text':
+            ctx.type = 'text/plain';
+            if ('development' == env) ctx.body = err.message
+            else if (err.expose) ctx.body = err.message
+            else throw err;
+            break;
 
-        case 'json':
-          this.type = 'application/json';
-          if ('development' == env) this.body = { error: err.message }
-          else if (err.expose) this.body = { error: err.message }
-          else this.body = { error: http.STATUS_CODES[this.status] }
-          break;
+          case 'json':
+            ctx.type = 'application/json';
+            if ('development' == env) ctx.body = { error: err.message }
+            else if (err.expose) ctx.body = { error: err.message }
+            else ctx.body = { error: http.STATUS_CODES[ctx.status] }
+            break;
 
-        case 'html':
-          this.type = 'text/html';
-          this.body = yield render(path, {
-            engine: engine,
-            cache: cache,
-            env: env,
-            ctx: this,
-            request: this.request,
-            response: this.response,
-            error: err.message,
-            stack: err.stack,
-            status: this.status,
-            code: err.code
-          });
-          break;
-      }
-    }
+          case 'html':
+            ctx.type = 'text/html';
+            return consolidate[engine](path, {
+              cache: cache,
+              env: env,
+              ctx: ctx,
+              request: ctx.request,
+              response: ctx.response,
+              error: err.message,
+              stack: err.stack,
+              status: ctx.status,
+              code: err.code
+            }).then(body => {
+              ctx.body = body;
+            });
+        }
+      });
   }
 }
